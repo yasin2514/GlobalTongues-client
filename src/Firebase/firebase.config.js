b28da8924a7d2ad371596b63ef54fe798a6d2472
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-06-10 23:02:09 +0600

    env varoables protedted

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDEuUkK7Lj18vdjFp_gEhP9F6ZgeKpbztk",
-    authDomain: "globaltongues-e31c8.firebaseapp.com",
-    projectId: "globaltongues-e31c8",
-    storageBucket: "globaltongues-e31c8.appspot.com",
-    messagingSenderId: "177991091001",
-    appId: "1:177991091001:web:7fcb956781d4001c4f5ee8"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
