191d2acda2475ac473c7cbb7890de7f87ccb014a
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-06-12 17:48:51 +0600

    user pfofile dapdated

--- src/Components/userProfile/UserProfile.jsx
+++ src/Components/userProfile/UserProfile.jsx
@@ -5,28 +5,25 @@ const UserProfile = () => {
     const { user } = useContext(AuthContext);
     return (
         <>
-            <div className="text-center">
+            <div className=" text-center">
                 <h className="text-4xl font-bold">Welcome <span className="text-orange-600">{user.displayName}</span></h>
-                <p className="text-[16px] text-blue-600">Manage dashboard & here is the below your profile Information</p>
             </div>
-
-            <div className="mt-20 flex items-center justify-center gap-10">
+            <div className="flex flex-col items-center mt-10">
                 <div className="avatar online">
                     <div className="w-52 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                         <img src={user.photoURL} />
                     </div>
                 </div>
-                <div className="text-xl mt-5 space-y-3 w-1/2 pr-10">
-                    <p className=" font-bold">Name: <span className="font-semibold">{user.displayName}</span></p>
+                <div className="text-center mt-10">
                     <p className=" text-xl">Email: {user.email}</p>
-                    <p className=""><span className="text-xl text-orange-600">{user.displayName}</span> have curious mind yearning for knowledge and boundless creativity. Embracing challenges, seeking growth, and embracing every moment's beauty. Avid traveler, soaking in diverse cultures, unraveling stories of faraway lands. Inspiring others to dream big, while savoring the small joys of life. Embracing the world's tapestry, one adventure at a time.</p>
+                    <p className="px-10"><span className="text-xl text-orange-600">{user.displayName}</span> have curious mind yearning for knowledge and boundless creativity. Embracing challenges, seeking growth, and embracing every moment's beauty. Avid traveler, soaking in diverse cultures, unraveling stories of faraway lands. Inspiring others to dream big, while savoring the small joys of life. Embracing the world's tapestry, one adventure at a time.</p>
+                    <p className="text-[16px] mt-20 text-blue-600">Manage your dashboard & stay connected with Us</p>
+                    <p className="text-green-600 italic">---Stay Connect With Us---</p>
+                    <p className=" text-orange-600">Team </p>
+                    <p className="text-3xl font-bold text-blue-600">Global Tongues</p>
                 </div>
             </div>
 
-            <div className="mt-20">
-                <p className="text-green-600 italic text-center">---Stay Connect With Us---</p>
-                <span className="text-center text-orange-600">Team <p className="text-3xl font-bold text-blue-600">Global Tongues</p></span>
-            </div>
         </ >
     );
 };
